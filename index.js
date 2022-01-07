abd482469ed8f19f2df51bee46874ab25237e8bc
hari1409 <84219722+Harine1409@users.noreply.github.com>
2022-01-07 14:22:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+   
+  "mongodb+srv://admin:harine@cluster0.4rwyh.mongodb.net/merntutorial?retryWrites=true&w=majority"
+ 
+)
+console.log("mongo connected")
+;
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3003, () => {
+  console.log("SERVER RUNS PERFECTLY!");
+});
